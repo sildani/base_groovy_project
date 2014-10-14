c715622b685aa4fd668b92cb24385a0fb981d9d3
Daniel Silva <daniel@silvanolte.com>
2014-10-14 12:28:55 -0400

    Initial commit.

---
+++ bin/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# replace 'MainClass' with the actual main class name
+java -cp "gen/classes:lib/*" MainClass
---
+++ test/unit/MainClassTest.groovy
@@ -0,0 +1,11 @@
+import org.junit.*
+
+public class MainClassTest {
+
+  @Test
+  public void should_return_true() {
+    MainClass.main([] as String[])
+    assert true
+  }
+
+}
