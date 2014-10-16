0a1fa444720fed252e74859605841cb9330cfce4
Daniel Silva <daniel@silvanolte.com>
2014-10-16 16:17:58 -0400

    Updated unit test case to demonstrate how to test for logged messages (something I find very useful.

--- test/unit/MainClassTest.groovy
+++ test/unit/MainClassTest.groovy
@@ -1,11 +1,30 @@
-import org.junit.*
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.appender.ArrayAppender
+import org.junit.Before
+import org.junit.Test
 
 public class MainClassTest {
+
+  def logAppender
+  def configFile
+
+  public MainClassTest() {
+    def logger = LogManager.getLogger(MainClass.class.name)
+    logAppender = new ArrayAppender('TestAppender',null,null)
+    logAppender.start()
+    logger.addAppender(logAppender)
+  }
+
+  @Before
+  public void setup() {
+    configFile = File.createTempFile('main_class_','.properties')
+    logAppender.clearMessages()
+  }
 
   @Test
   public void should_return_true() {
     MainClass.main([] as String[])
-    assert true
+    assert logAppender.received('INFO - This is the main class.')
   }
 
 }
