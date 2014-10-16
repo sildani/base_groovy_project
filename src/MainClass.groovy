893f97e5c7e8906670240199869e8d6019ea92f6
Daniel Silva <daniel@silvanolte.com>
2014-10-16 16:19:23 -0400

    cleaned up the imports on the MainClass groovy file.

--- src/MainClass.groovy
+++ src/MainClass.groovy
@@ -1,9 +1,8 @@
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 class MainClass {
 
-  private static final Logger logger = LogManager.getLogger(MainClass.class.name);
+  private static final def logger = LogManager.getLogger(MainClass.class.name);
 
   public static void main(String[] args) {
     logger.info("This is the main class.")
